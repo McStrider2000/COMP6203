34c7e65be4baf2ffecc1f6d1afa74662ce6f7345
Chris <cjm1n19@soton.ac.uk>
2024-11-07 17:59:25 +0000

    Exercise 2.1 I think - they don't tell us what it should be outputting

--- TradingCompany/MyCompany.py
+++ TradingCompany/MyCompany.py
@@ -1,14 +1,30 @@
 from mable.cargo_bidding import TradingCompany
+from mable.transport_operation import ScheduleProposal
 from mable.examples import environment, fleets
-
+import itertools
 
 class MyCompany(TradingCompany):
-    pass
+    def propose_schedules(self, trades):
+        schedules = {}
+        costs = {}
+        scheduled_trades = []
+        for vessel in self.fleet:
+            curr_schedule = vessel.schedule
+            for trade in trades:
+                # if trade not in scheduled_trades:
+                copied = curr_schedule.copy()
+                copied.add_transportation(trade)
+                is_valid = copied.verify_schedule()
+                if is_valid:
+                    curr_schedule = copied
+                    scheduled_trades.append(trade)
+            schedules[vessel] = curr_schedule
 
+        return ScheduleProposal(schedules, scheduled_trades)
 
 if __name__ == '__main__':
     specifications_builder = environment.get_specification_builder(environment_files_path="../resources")
-    fleet = fleets.example_fleet_1()
+    fleet = fleets.example_fleet_2()
     specifications_builder.add_company(MyCompany.Data(MyCompany, fleet, "Shipping Corp Ltd."))
     sim = environment.generate_simulation(specifications_builder)
     sim.run()
