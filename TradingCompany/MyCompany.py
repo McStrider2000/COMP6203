bce764d6f7073c9526341e2c067235be45513e93
Chris <cjm1n19@soton.ac.uk>
2024-11-08 15:24:56 +0000

    WIP - bidding update

--- TradingCompany/MyCompany.py
+++ TradingCompany/MyCompany.py
@@ -5,6 +5,9 @@ from mable.transport_operation import ScheduleProposal
 from mable.examples import environment, fleets
 import itertools
 
+from pandas.core.config_init import pc_width_doc
+
+
 class MyCompany(TradingCompany):
     def propose_schedules(self, trades):
         schedules = {}
@@ -15,28 +18,44 @@ class MyCompany(TradingCompany):
             curr_schedule = vessel.schedule
             for trade in trades:
                 if trade not in scheduled_trades:
-                    new_schedule = curr_schedule.copy()
-                    insertion_points = new_schedule.get_insertion_points()
-                    shortest_schedule = None
-                    for i in range(len(insertion_points)):
-                        idx_pick_up = insertion_points[i]
-                        possible_drop_offs = insertion_points[i:]
-                        for j in range(len(possible_drop_offs)):
-                            idx_drop_off = possible_drop_offs[j]
-                            schedule_option = new_schedule.copy()
-                            schedule_option.add_transportation(trade, idx_pick_up, idx_drop_off)
-                            if shortest_schedule is None or schedule_option.completion_time() < shortest_schedule.completion_time():
-                                if schedule_option.verify_schedule():
-                                    shortest_schedule = schedule_option
+                    self.estimate_cost(curr_schedule, trade, vessel)
+                    shortest_schedule = self.find_shortest_schedule(curr_schedule.copy(), trade)
                     if shortest_schedule is not None:
-                        curr_schedule = new_schedule
+                        curr_schedule = shortest_schedule
                         scheduled_trades.append(trade)
             schedules[vessel] = curr_schedule
         return ScheduleProposal(schedules, scheduled_trades)
 
+    @staticmethod
+    def find_shortest_schedule(schedule, trade):
+        insertion_points = schedule.get_insertion_points()
+        shortest_schedule = None
+        for i in range(len(insertion_points)):
+            idx_pick_up = insertion_points[i]
+            possible_drop_offs = insertion_points[i:]
+            for j in range(len(possible_drop_offs)):
+                idx_drop_off = possible_drop_offs[j]
+                schedule_option = schedule.copy()
+                schedule_option.add_transportation(trade, idx_pick_up, idx_drop_off)
+                if shortest_schedule is None or schedule_option.completion_time() < shortest_schedule.completion_time():
+                    if schedule_option.verify_schedule():
+                        shortest_schedule = schedule_option
+        return shortest_schedule
+
+    def estimate_cost(self, schedule, trade, vessel):
+        pick_up_port = trade.origin_port
+        current_location = vessel.location
+
+        vessel.get_travel_time()
+        time_to_load = vessel.get_loading_time(trade.cargo_type, trade.amount)
+        loading_cost = vessel.get_loading_consumption(time_to_load)
+        print(loading_cost)
+        return 0
+
+
 if __name__ == '__main__':
     specifications_builder = environment.get_specification_builder(environment_files_path="../resources")
-    fleet = fleets.example_fleet_2()
+    fleet = fleets.example_fleet_1()
     specifications_builder.add_company(MyCompany.Data(MyCompany, fleet, "Shipping Corp Ltd."))
     sim = environment.generate_simulation(specifications_builder)
     sim.run()
