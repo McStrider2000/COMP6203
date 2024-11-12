0a5a590116274b3fbf50dfa2bbf3031cc550ec71
Chris <cjm1n19@soton.ac.uk>
2024-11-12 15:26:06 +0000

    inverted propose schedules for loops, should now test each vessel per trade to find the shortest schedule

--- TradingCompany/MyCompany.py
+++ TradingCompany/MyCompany.py
@@ -13,18 +13,22 @@ class MyCompany(TradingCompany):
         costs = {}
         scheduled_trades = []
 
-        for vessel in self.fleet:
-            curr_schedule = vessel.schedule
-            for trade in trades:
+        for trade in trades:
+            chosen_vessel = None
+            shortest_schedule = None
+            for vessel in self.fleet:
+                curr_schedule = schedules.get(vessel, vessel.schedule)
                 if trade not in scheduled_trades:
-                    # TODO: Improve cost estimation to run on each possible schedule route
-                    # TODO: Do we choose shortest or cheapest?
-                    cost = self.estimate_cost(curr_schedule, trade, vessel)
-                    shortest_schedule = self.find_shortest_schedule(curr_schedule.copy(), trade)
-                    if shortest_schedule is not None:
-                        curr_schedule = shortest_schedule
-                        scheduled_trades.append(trade)
-            schedules[vessel] = curr_schedule
+                    vessel_shortest_schedule = self.find_shortest_schedule(curr_schedule.copy(), trade)
+                    if vessel_shortest_schedule is not None:
+                        if (shortest_schedule is None) or (vessel_shortest_schedule.completion_time() < shortest_schedule.completion_time()):
+                            shortest_schedule = vessel_shortest_schedule
+                            chosen_vessel = vessel
+            if shortest_schedule is not None:
+                scheduled_trades.append(trade)
+                schedules[chosen_vessel] = shortest_schedule
+                costs[trade] = self.estimate_cost(trade, chosen_vessel)
+
         return ScheduleProposal(schedules, scheduled_trades, costs)
 
     @staticmethod
@@ -43,19 +47,17 @@ class MyCompany(TradingCompany):
                         shortest_schedule = schedule_option
         return shortest_schedule
 
-    def estimate_cost(self, schedule, trade, vessel):
-        hq = self.headquarters
-        pick_up_travel_cost = self.calculate_travel_cost(hq, vessel, vessel.location, trade.origin_port)
+    def estimate_cost(self, trade, vessel):
+        pick_up_travel_cost = self.calculate_travel_cost(vessel, vessel.location, trade.origin_port)
         time_to_load = vessel.get_loading_time(trade.cargo_type, trade.amount)
         loading_cost = vessel.get_loading_consumption(time_to_load)
-        drop_off_travel_cost = self.calculate_travel_cost(hq, vessel, trade.origin_port, trade.destination_port)
+        drop_off_travel_cost = self.calculate_travel_cost(vessel, trade.origin_port, trade.destination_port)
         unloading_cost = vessel.get_unloading_consumption(time_to_load)
 
         return pick_up_travel_cost + loading_cost + drop_off_travel_cost + unloading_cost
 
-    @staticmethod
-    def calculate_travel_cost(hq, vessel, location_a, location_b, is_laden = False):
-        distance_to_pickup = hq.get_network_distance(location_a, location_b)
+    def calculate_travel_cost(self, vessel, location_a, location_b, is_laden = False):
+        distance_to_pickup = self.headquarters.get_network_distance(location_a, location_b)
         time_to_pick_up = vessel.get_travel_time(distance_to_pickup)
         if is_laden:
             return vessel.get_laden_consumption(time_to_pick_up, vessel.speed)
@@ -64,7 +66,7 @@ class MyCompany(TradingCompany):
 
 
 if __name__ == '__main__':
-    specifications_builder = environment.get_specification_builder(environment_files_path="../resources")
+    specifications_builder = environment.get_specification_builder(environment_files_path="../resources", trades_per_occurrence=2)
     fleet = fleets.example_fleet_1()
     specifications_builder.add_company(MyCompany.Data(MyCompany, fleet, "Shipping Corp Ltd."))
     sim = environment.generate_simulation(specifications_builder)
